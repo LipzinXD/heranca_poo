5ab3a535f96239b312521c30b7c06a429277bcda
LipzinXD <felipescamargo806@gmail.com>
2023-04-18 13:54:19 +0000

    Herança

---
+++ Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        
+        Animal animal = new Animal();
+        Cachorro cachorro = new Cachorro();
+        animal.som();//
+        cachorro.som();//
+    }
+}
